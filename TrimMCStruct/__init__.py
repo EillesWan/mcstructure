f01336a29414f620d21839533c66dbc0c88dc05e
Eilles <71250018+EillesWan@users.noreply.github.com>
2023-04-02 00:40:20 +0800

    Delete TrimMCStruct directory

--- TrimMCStruct/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Read and write Minecraft .mcstructure files.
-"""
-
-__version__ = "0.0.4"
-__author__ = (("金羿", "Eilles Wan"), ("诸葛亮与八卦阵", "bgArray"), ("phoenixR", "phoenixR"))
-
-from .main import *
