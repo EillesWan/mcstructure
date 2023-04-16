0a7b735169c4fd507757b6477073f08b4bd6f4cd
EillesWan <W-YI_DoctorYI@outlook.com>
2023-04-16 18:20:03 +0800

    更新一些内容

--- TrimMCStruct/__init__.py
+++ TrimMCStruct/__init__.py
@@ -1,5 +1,14 @@
 """
 Read and write Minecraft .mcstructure files.
+
+版权所有 © 2023 全体 TriMCStruct 开发者
+Copyright © 2023 all the developers of TriMCStruct
+
+开源相关声明请见 ../LICENSE.md
+Terms & Conditions: ../LICENSE.md
+
+
+
 """
 
 __version__ = "0.0.4"
